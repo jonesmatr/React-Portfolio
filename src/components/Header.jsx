91ebfa33e26b4e4e72d3174bdc4fc50361c1c5da
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-10-06 07:32:57 -0400

    Added code for navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import Navigation from './Navigation';
 const Header = () => {
   return (
     <header>
-      <h1>Developer Name</h1>
+      <h1>Matthew R. Jones</h1>
       <Navigation />
     </header>
   );
