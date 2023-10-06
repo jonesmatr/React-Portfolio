2abf5b509edb6eccb7b9d1ae09bd321aae5f4bba
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-10-06 12:53:41 -0400

    Added contact, footer, and project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header.jsx';
+import AboutMe from './components/AboutMe.jsx';
+import Portfolio from './components/Portfolio.jsx';
+import Contact from './components/Contact.jsx';
+import Resume from './components/Resume.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main>
+        <AboutMe />
+        <Portfolio />
+        <Contact />
+        <Resume />
+      </main>
+      <Footer />
     </div>
   );
 }
