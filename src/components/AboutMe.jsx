59de27d21803196b620eb3fc8ff6d6c1512de92b
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-10-08 09:12:27 -0400

    Added bio and profile image. Also, adjusted the styling

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,11 +2,25 @@ import React from 'react';
 
 const AboutMe = () => {
   return (
-    <section id="about-me">
-      <img src="avatar.png" alt="Developer" />
-      <p>Short bio about the developer.</p>
+    <section id="about-me" className="about-me-container">
+      <h1>About Me</h1>
+      <img src="profile-pic.jpg" alt="Developer" className="profile-image" />
+      <p className="bio">Matthew is a multifaceted and dedicated developer with a decade of on-and-off experience in building intuitive and innovative websites. With a skill set deeply rooted in React, JavaScript, Express.js, and MongoDB, Matthew has a proven track record of transforming complex challenges into seamless web solutions.
+
+Being self-employed has equipped Matthew with a diverse perspective on web development, granting him the flexibility and creativity to craft bespoke web solutions tailored to meet the unique needs of each client. His ability to blend technical expertise with artistic flair is evident in the aesthetic and functional excellence of every website he touches.
+
+Matthew's journey in web development is complemented by his passion for architectural drawing and house flipping. This blend of technical and creative pursuits has not only enriched his professional outlook but has also enhanced his ability to view each project as a unique piece of art, deserving of attention to every detail.
+
+Whether he's writing a new line of code or sketching the blueprint for a home renovation, Matthew's commitment to excellence and innovation remains the constant. Each website is not just a set of pages but a space, intricately crafted to ensure that every visitor's experience is not just seamless but also memorable.
+
+Matthew is always open to exploring new opportunities and collaborations that challenge him to push beyond the conventional. In a world that's always changing, he remains committed to staying ahead, always learning, and always innovating.
+
+
+</p>
     </section>
   );
 };
+
+
 
 export default AboutMe;
