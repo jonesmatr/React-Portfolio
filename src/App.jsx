a609dd2b2cbae992e2f0baaac5f80c704a2680be
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-10-08 07:39:07 -0400

    Added styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
 import Header from './components/Header';
-import Footer from './components/Footer';
 import AboutMe from './components/AboutMe';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <AboutMe />
-      <Portfolio />
-      <Contact />
-      <Resume />
+      <Header /> {/* Navigation is included within the Header component */}
+
+      <main>
+        <section id="about" className="bg-light p-5 rounded">
+          <AboutMe />
+        </section>
+
+        <section id="portfolio" className="bg-secondary p-5 rounded my-5">
+          <Portfolio />
+        </section>
+
+        <section id="contact" className="bg-light p-5 rounded">
+          <Contact />
+        </section>
+
+        <section id="resume" className="bg-secondary p-5 rounded my-5">
+          <Resume />
+        </section>
+      </main>
+
       <Footer />
     </div>
   );
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,13 +2,26 @@ import React from 'react';
 
 const Navigation = () => {
   return (
-    <nav>
-      <ul>
-        <li>About Me</li>
-        <li>Portfolio</li>
-        <li>Contact</li>
-        <li>Resume</li>
-      </ul>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Home</a>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#about">About Me</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#portfolio">Portfolio</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#contact">Contact</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#resume">Resume</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
